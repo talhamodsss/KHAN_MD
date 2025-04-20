0691b4b0fccfa81ea397bf4bed899d09ced2d560
KHAN MD <157266962+XdTechPro@users.noreply.github.com>
2025-04-20 21:12:45 +0500

    Update tool-repeat.js

--- plugins/tool-repeat.js
+++ plugins/tool-repeat.js
@@ -3,7 +3,6 @@ const { cmd, commands } = require('../command');
 
 cmd({
     pattern: "msg",
-    alias: ["repeat", "spam"],
     desc: "Send a message multiple times (Owner Only)",
     category: "utility",
     react: "🔁",
